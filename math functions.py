0e108b3b4c208483acf6208513664ce031c7a458
cossa <alheli.coss@gmail.com>
2024-09-13 10:49:48 -0600

    factorial and reminder

--- math functions.py
+++ math functions.py
@@ -1,3 +1,14 @@
 def factorial (n):
-    result = 1
-    for i in range 
+    resultoffactorial = 1
+    for i in range == (2,n + 1):
+        resultoffactorial *= i
+    return resultoffactorial
+
+factorial(6)
+
+
+def reminder(a,b):
+    print(a-((a//b)*b))
+
+
+reminder(5,2)
